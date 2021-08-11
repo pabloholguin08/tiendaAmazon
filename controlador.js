f01f66da92a610a36aba512d8da4bbeadf881352
pabloholguin08 <jpholguin06@gmail.com>
2021-08-11 16:55:12 -0500

    Actualización 11/08/2021

--- controlador.js
+++ controlador.js
@@ -1,13 +1,17 @@
 let nombreConsola;
 let precioConsola;
 let pesoConsola;
-let fotoConsola
+let fotoConsola;
+
  
 let boton=document.getElementById("boton");
 let botonLimpiar=document.getElementById("botonLimpiar")
+let botonCop=document.getElementById("cop")
 
 boton.addEventListener("click", capturarDatos); //callback
 botonLimpiar.addEventListener("click", limpiarCarrito);
+botonCop.addEventListener("click", conversion);
+
 
  //
 function capturarDatos(){
@@ -16,20 +20,27 @@ function capturarDatos(){
     seleccionarConsola(producto);
 
     let cantidad=document.getElementById("cantidad").value;
-    
 
     let pildora=document.getElementById("pildora");
     pildora.textContent=cantidad;
     pildora.classList.remove("invisible");
     pildora.classList.add("visible")
+
+    let hr1=document.getElementById("hr1");
+    hr1.classList.remove("invisible");
+    hr1.classList.add("visible");
+
+    let hr2=document.getElementById("hr2");
+    hr2.classList.remove("invisible");
+    hr2.classList.add("visible");
     
     let direccion=document.getElementById("comentarios").value;
 
     let fotoCarrito=document.getElementById("fotoCarrito");
     fotoCarrito.src=fotoConsola;
 
-    let tituloCarrito=document.getElementById("tituloCarrito");
-    tituloCarrito.textContent=nombreConsola;
+    let tituloCarro=document.getElementById("tituloCarrito");
+    tituloCarro.textContent=nombreConsola;
 
     let precioIndividual=document.getElementById("precioUnitarioCarrito");
     precioIndividual.textContent=`Valor unidad: $${precioConsola}USD`;
@@ -48,8 +59,16 @@ function capturarDatos(){
 
     let costoTotal=document.getElementById("costoTotal");
     costoTotal.textContent=`Costo Total:$${(calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad))}USD`;
+
+    botonCop.classList.remove("invisible");
+    botonCop.classList.add("visible");
+    
 }
 
+function conversion(){
+    let pesos=document.getElementById("costoPesos");
+    pesos.textContent=`Valor en pesos: $${convertirDolares((calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad)))}COP`;
+}
     
 
 function seleccionarConsola(opcion){
@@ -123,5 +142,44 @@ function convertirDolares(precioDolares){
 }
 
 function limpiarCarrito(){
+    
+    let fotoCarrito=document.getElementById("fotoCarrito");
+    fotoCarrito.src="https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/fotoCarrito.PNG?raw=true";
 
+    let tituloCarro=document.getElementById("tituloCarrito");
+    tituloCarro.textContent=("Tu carrito de compra está vacío");
+
+    let precioIndividual=document.getElementById("precioUnitarioCarrito");
+    precioIndividual.textContent=null;
+
+    let pesoIndividual=document.getElementById("pesoUnitarioCarrito");
+    pesoIndividual.textContent=null;
+
+    let pesoTotal=document.getElementById("pesoTotalCarrito");
+    pesoTotal.textContent=null;
+
+    let casillero=document.getElementById("costoCasillero");
+    casillero.textContent=null;
+
+    let impuestos=document.getElementById("impuestos");
+    impuestos.textContent=null;
+
+    let costoTotal=document.getElementById("costoTotal");
+    costoTotal.textContent=null;
+
+    let pesos=document.getElementById("costoPesos");
+    pesos.textContent=null;
+
+    let pildora=document.getElementById("pildora");
+    pildora.textContent=cantidad;
+    pildora.classList.remove("visible");
+    pildora.classList.add("invisible");
+
+    let hr1=document.getElementById("hr1");
+    hr1.classList.remove("visible");
+    hr1.classList.add("invisible");
+
+    let hr2=document.getElementById("hr2");
+    hr2.classList.remove("visible");
+    hr2.classList.add("invisible");
 }
