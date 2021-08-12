29a491b678797f017deed9abf9a258356a904a0c
pabloholguin08 <jpholguin06@gmail.com>
2021-08-12 16:41:57 -0500

    Conversiones 12/08/2021

--- controlador.js
+++ controlador.js
@@ -5,12 +5,14 @@ let fotoConsola;
 
  
 let boton=document.getElementById("boton");
-let botonLimpiar=document.getElementById("botonLimpiar")
-let botonCop=document.getElementById("cop")
+let botonLimpiar=document.getElementById("botonLimpiar");
+let botonCop=document.getElementById("cop");
+let botonUsd=document.getElementById("usd");
 
 boton.addEventListener("click", capturarDatos); //callback
 botonLimpiar.addEventListener("click", limpiarCarrito);
 botonCop.addEventListener("click", conversion);
+botonUsd.addEventListener("click", conversionDolar);
 
 
  //
@@ -62,21 +64,58 @@ function capturarDatos(){
 
     botonCop.classList.remove("invisible");
     botonCop.classList.add("visible");
-    
 }
 
 function conversion(){
-    let pesos=document.getElementById("costoPesos");
-    pesos.textContent=`Valor en pesos: $${convertirDolares((calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad)))}COP`;
+    let cantidad=document.getElementById("cantidad").value;
+
+    let precioIndividual=document.getElementById("precioUnitarioCarrito");
+    precioIndividual.textContent=`Valor unidad: $${convertirDolares(precioConsola)}COP`;
+
+    let casillero=document.getElementById("costoCasillero");
+    casillero.textContent=`Valor Casillero: $${convertirDolares(calcularCostoCasillero(pesoConsola,cantidad))}COP`;
+
+    let impuestos=document.getElementById("impuestos");
+    impuestos.textContent=`Costo venta(Impuestos): $${convertirDolares(calcularCostoImpuestos(precioConsola, cantidad))}COP`;
+
+    let costoTotal=document.getElementById("costoTotal");
+    costoTotal.textContent=`Valor en pesos: $${convertirDolares((calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad)))}COP`;
+
+    botonCop.classList.remove("visible");
+    botonCop.classList.add("invisible");
+
+    botonUsd.classList.remove("invisible");
+    botonUsd.classList.add("visible");
 }
     
+function conversionDolar(){
+    let cantidad=document.getElementById("cantidad").value;
+
+    let precioIndividual=document.getElementById("precioUnitarioCarrito");
+    precioIndividual.textContent=`Valor unidad: $${convertirPesos(convertirDolares(precioConsola))}USD`;
+
+    let casillero=document.getElementById("costoCasillero");
+    casillero.textContent=`Valor Casillero: $${convertirPesos(convertirDolares(calcularCostoCasillero(pesoConsola,cantidad)))}USD`;
+
+    let impuestos=document.getElementById("impuestos");
+    impuestos.textContent=`Costo venta(Impuestos): $${convertirPesos(convertirDolares(calcularCostoImpuestos(precioConsola, cantidad)))}USD`;
+
+    let costoTotal=document.getElementById("costoTotal");
+    costoTotal.textContent=`Valor en pesos: $${convertirPesos(convertirDolares((calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad))))}USD`;
+
+    botonCop.classList.remove("invisible");
+    botonCop.classList.add("visible");
+
+    botonUsd.classList.remove("visible");
+    botonUsd.classList.add("invisible");
+}
 
 function seleccionarConsola(opcion){
     let consolas={
-        nombres:Array("PS5","XBOX Serie X", "Nintendo Switch"),
-        precios:Array(750,700,409),
-        pesos:Array(14.2,13.1,4.75),
-        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true"),
+        nombres:Array("PS5","XBOX Serie X", "Nintendo Switch","Huion Kanvas 13"),
+        precios:Array(750,700,409,239),
+        pesos:Array(14.2,13.1,4.75,2),
+        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true","https://github.com/pabloholguin08/fotoss/blob/main/tableta.jpg?raw=true"),
         
     }
 
@@ -100,6 +139,13 @@ function seleccionarConsola(opcion){
         precioConsola=consolas.precios[2];
         pesoConsola=consolas.pesos[2];
         fotoConsola=consolas.fotos[2];
+
+    }else if(opcion==4){
+
+        nombreConsola=consolas.nombres[3];
+        precioConsola=consolas.precios[3];
+        pesoConsola=consolas.pesos[3];
+        fotoConsola=consolas.fotos[3];
 
     }else{
         nombreConsola=null;
@@ -135,10 +181,17 @@ function calcularCostoImpuestos(precioConsola,cantidad){
     
 function convertirDolares(precioDolares){
 
-    const TRM=3932; //3932 pesos equivalen a 1 dolar
+    const TRM=3859; //3932 pesos equivalen a 1 dolar
     let precioPesos= precioDolares*TRM;
 
     return precioPesos;
+}
+
+function convertirPesos(precioPesos){
+    const TRM=3859;
+    let precioDolares = precioPesos/TRM;
+
+    return precioDolares;
 }
 
 function limpiarCarrito(){
@@ -182,4 +235,7 @@ function limpiarCarrito(){
     let hr2=document.getElementById("hr2");
     hr2.classList.remove("visible");
     hr2.classList.add("invisible");
+
+    botonCop.remove("visible")
+    botonCop.add("invisible");
 }
